9b8a514fc003c3b14356189850a3fc79c25d7b64
Raj, Joseph <rajjos@wahbexchange.org>
2020-05-21 15:54:41 -0700

    Add project files.

---
+++ SampleGitAction/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SampleGitAction.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
